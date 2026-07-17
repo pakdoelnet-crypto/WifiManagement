12fba228884ef241171e61172f4fce41177e92dc
Your Name <you@example.com>
2026-07-18 01:23:35 +0700

    Expose Nginx error logs in deploy webhook

--- routes/web.php
+++ routes/web.php
@@ -226,8 +226,12 @@ Route::post('/deploy-webhook', function (\Illuminate\Http\Request $request) {
     $psOutput = [];
     exec('ps aux | grep php 2>&1', $psOutput);
 
+    $nginxLogs = [];
+    exec('tail -n 50 /var/log/nginx/error.log 2>&1', $nginxLogs);
+
     return response()->json([
         'success' => true,
+        'nginx_error_logs' => $nginxLogs,
         'cache_details' => $cacheDetails,
         'processes' => $psOutput,
         'output' => $output
